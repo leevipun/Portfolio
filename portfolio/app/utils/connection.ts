1bf757bcfa89c01a03e923f99c050f0653a6c0f8
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2024-01-21 22:34:09 +0200

    right env

--- portfolio/app/utils/connection.ts
+++ portfolio/app/utils/connection.ts
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const connect = async () => {
   if (mongoose.connection.readyState) {
     return;
   }
   try {
-    await mongoose.connect(
-      'mongodb+srv://leevipuntanen2:cq2IbTQFJ8CHtwDU@portfolio.s7pmwxf.mongodb.net/Projects?retryWrites=true&w=majority'
-    );
+    await mongoose.connect(MONGODB_URI as string);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.log(error);
